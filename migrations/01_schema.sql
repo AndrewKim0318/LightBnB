c7fc41b750ca7160e87159c41c9222cb62898696
Andrew Kim <wk@ualberta.ca>
2019-08-21 14:40:10 +0000

    initial attempt in creating the tables for lightbnb

---
+++ migrations/01_schema.sql
@@ -0,0 +1,59 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+)
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) NOT NULL,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night VARCHAR(255),
+  street VARCHAR(255),
+  parking_spaces SMALLINT,
+  number_of_bathrooms SMALLINT,
+  number_of_bedrooms SMALLINT,
+  country VARCHAR (255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(32),
+  active BOOLEAN DEFAULT TRUE
+)
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  property_id INTEGER REFERENCES properties(id) NOT NULL,
+  start_date DATE,
+  end_date DATE,
+  cost_per_night SMALLINT
+)
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) NOT NULL,
+  property_ID INTEGER REFERENCES properties(id) NOT NULL,
+  start_date DATE,
+  end_date DATE 
+)
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) NOT NULL,
+  reservation_id INTEGER REFERENCES reservations(id) NOT NULL,
+  property_id INTEGER REFERENCES properties(id) NOT NULL,
+  rating SMALLINT,
+  message TEXT
+)
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) NOT NULL,
+  owner_id INTEGER REFERENCES users(id) NOT NULL,
+  reservation_id INTEGER REFERENCES reservations(id) NOT NULL,
+  rating SMALLINT,
+  message TEXT
+)

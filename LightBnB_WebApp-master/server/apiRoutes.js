ab62cfde7c3e545afb1ff7c548fcf9c690181115
Andrew Kim <wk@ualberta.ca>
2019-08-22 20:08:18 +0000

    moved all the queries from database.js into userRoutes and apiRoutes and changed the database.js file to export the query function

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -1,7 +1,69 @@
 module.exports = function(router, database) {
+  const db = require('./database');
 
   router.get('/properties', (req, res) => {
-    database.getAllProperties(req.query, 20)
+
+    // Create values array
+    const queryParams = [];
+    // Create text string
+    let queryString = `
+    SELECT properties.*, AVG(property_reviews.rating) as average_rating
+    FROM properties
+    LEFT JOIN property_reviews ON properties.id = property_id
+    `;
+
+    // Set conditions
+    // If city parameter is in options
+    if (req.query.city) {
+      queryParams.push(`%${req.query.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+
+    // If owner_id parameter is in options, only return properties belonging to the owner
+    if (req.query.guest_id) {
+      queryParams.push(req.query.guest_id);
+      if (queryParams.length === 1) {
+        queryString += `WHERE properties.owner_id = $${queryParams.length}`;
+      } else {
+        queryString += `AND properties.owner_id = $${queryParams.length}`;
+      }
+    }
+    
+    // If minimum_price_per_night AND maximum_price_per_night is in options, only return properties within that price range
+    if (req.query.minimum_price_per_night && req.query.maximum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night);
+      queryParams.push(options.maximum_price_per_night);
+
+      if(queryParams.length === 2) {
+        queryString += `WHERE cost_per_night > $${(queryParams.length - 1)} AND cost_per_night < $${(queryParams.length)}`; 
+      } else {
+        queryString += `AND cost_per_night > $${(queryParams.length - 1)} AND cost_per_night < $${(queryParams.length)}`;
+      }
+    }
+    // If minimum_rating is in options, only return properties with a rating equal or higher than that
+    if (req.query.minimum_rating) {
+      queryParams.push(Number(req.query.minimum_rating));
+      queryString += `
+      GROUP BY properties.id
+      HAVING AVG(property_reviews.rating) >= $${queryParams.length}
+      `;
+      queryParams.push(20);
+      queryString += `
+      ORDER BY cost_per_night
+      LIMIT $${queryParams.length};
+      `;
+    } else { // Add the number of properties you want to see
+      queryParams.push(20);
+      queryString += `
+      GROUP BY properties.id
+      ORDER BY cost_per_night
+      LIMIT $${queryParams.length};
+      `;
+    }
+
+    db.query(queryString, queryParams)
+     // database.getAllProperties(req.query, 20)
+    .then(res => res.rows)
     .then(properties => {
       res.send({properties})
     })
@@ -17,7 +79,23 @@ module.exports = function(router, database) {
       res.error("💩");
       return;
     }
-    database.getAllReservations(userId)
+    const queryString = `
+    SELECT
+    reservations.*,
+    properties.*,
+    AVG(property_reviews.rating) AS average_rating
+    FROM properties
+      JOIN reservations ON properties.id = reservations.property_id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 AND reservations.end_date < now()::DATE
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date DESC LIMIT $2;
+    `;
+    const queryParams = [userId, 10];
+
+    // database.getAllReservations(userId)
+    db.query(queryString, queryParams)
+    .then(res => res.rows)
     .then(reservations => {
       res.send({reservations});
     })
@@ -29,14 +107,38 @@ module.exports = function(router, database) {
 
   router.post('/properties', (req, res) => {
     const userId = req.session.userId;
-    database.addProperty({...req.body, owner_id: userId})
-      .then(property => {
-        res.send(property);
-      })
-      .catch(e => {
-        console.error(e);
-        res.send(e)
-      });
+    const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `;
+    const queryParams = [
+      req.body.owner_id,
+      req.body.title,
+      req.body.description,
+      req.body.thumbnail_photo_url,
+      req.body.cover_photo_url,
+      req.body.cost_per_night,
+      req.body.parking_spaces,
+      req.body.number_of_bathrooms,
+      req.body.number_of_bedrooms,
+      req.body.country,
+      req.body.street,
+      req.body.city,
+      req.body.province,
+      req.body.post_code,
+    ];
+    
+    // database.addProperty({...req.body, owner_id: userId})
+    db.query(queryString, queryParams)
+    .then(res => res.rows)
+    .then(property => {
+      res.send(property);
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e)
+    });
   });
 
   return router;
--- LightBnB_WebApp-master/server/userRoutes.js
+++ LightBnB_WebApp-master/server/userRoutes.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const db = require('./database');
 
 module.exports = function(router, database) {
 
@@ -6,7 +7,17 @@ module.exports = function(router, database) {
   router.post('/', (req, res) => {
     const user = req.body;
     user.password = bcrypt.hashSync(user.password, 12);
-    database.addUser(user)
+    
+    
+    // database.addUser(user)
+    const queryString = `
+    INSERT INTO users(name, email, password)
+    VALUES ($1, $2, $3);
+    RETURNING *
+    `;
+    const queryParams = [user.name, user.email, user.password];
+    
+    db.query(queryString, queryParams)
     .then(user => {
       if (!user) {
         res.send({error: "error"});
@@ -24,7 +35,17 @@ module.exports = function(router, database) {
    * @param {String} password encrypted
    */
   const login =  function(email, password) {
-    return database.getUserWithEmail(email)
+    // return database.getUserWithEmail(email)
+
+    const queryString = `
+    SELECT *
+    FROM users
+    WHERE email = $1;
+    `;
+    const queryParams = [email];
+    
+    db.query(queryString, queryParams)
+    .then (res => res.rows[0])
     .then(user => {
       if (bcrypt.compareSync(password, user.password)) {
         return user;
@@ -60,15 +81,24 @@ module.exports = function(router, database) {
       return;
     }
 
-    database.getUserWithId(userId)
-      .then(user => {
-        if (!user) {
-          res.send({error: "no user with that id"});
-          return;
-        }
-        res.send({user: {name: user.name, email: user.email, id: userId}});
-      })
-      .catch(e => res.send(e));
+    // database.getUserWithId(userId)
+    const queryString = `
+    SELECT *
+    FROM users
+    WHERE id = $1;
+    `;
+    const queryParams = [userId];
+    
+    db.query(queryString, queryParams)
+    .then (res => res.rows[0] )
+    .then(user => {
+      if (!user) {
+        res.send({error: "no user with that id"});
+        return;
+      }
+      res.send({user: {name: user.name, email: user.email, id: userId}});
+    })
+    .catch(e => res.send(e));
   });
 
   return router;
